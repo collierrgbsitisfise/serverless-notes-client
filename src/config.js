71894c0a53f5f80d6332c7c4aefa037b294de621
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2020-04-18 22:16:25 +0300

    Setting up  React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "vadim-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://npb4hm361a.execute-api.eu-west-2.amazonaws.com/prod/notes"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_rFdRjix0R",
+    APP_CLIENT_ID: "2g70qq4ro8dq91ee9u5q6qtgvv",
+    IDENTITY_POOL_ID: "eu-west-2:e2b2dc3b-61ec-49cb-a6a9-df46b6d75e95"
+  }
+};
